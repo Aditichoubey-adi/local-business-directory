a56e57b18eef3388d98a45e7a4027f3bce1eb1c2
Aditichoubey-adi <jchoubey609@gmail.com>
2025-05-30 23:31:00 +0530

    Config: Adjust Render Root Directory and WSGI paths

--- local_business_directory/wsgi.py
+++ local_business_directory/wsgi.py
@@ -8,8 +8,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'local_business_directory.settings')
